79d9bdcef35e2d5719e6d215c4287e64a55d12fb
HuangBin <1976197555@qq.com>
2019-12-03 11:15:03 +0800

    One

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div @click="btn">
+      这是一个简单的home主页
+    </div> 
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+      
+      }
+        
+    },methods: {
+        btn(){
+         alert("我该怎么把它上传到GitHub 上呢  git add")
+        }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
